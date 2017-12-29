3d505e085e5dfdda7116035ba279ef2f4018e5ce
Milco Numan <milco.numan@gmail.com>
2017-12-29 21:30:13 +0100

    Playing around with mocha

---
+++ mocha-test/test/test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+describe('Array', function() {
+	  describe('#indexOf()', function() {
+		      it('should return -1 when the value is not present', function() {
+			            assert.equal([1,2,3].indexOf(4), -1);
+			          });
+		    });
+	  describe('#indexOf()', function() {
+		      it('should return 1 when the value is present at 0-based position 1', function() {
+			            assert.equal([1,2,3].indexOf(2), 1);
+			          });
+		    });
+});
